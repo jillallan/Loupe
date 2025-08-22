12d5158499720a29297ee1969c78c67112688925
Jill Riddell <54241079+jillallan@users.noreply.github.com>
2025-08-22 15:06:02 +0100

    Update image loading

--- Loupe/PhotoLibraryManager.swift
+++ Loupe/PhotoLibraryManager.swift
@@ -1,28 +1,72 @@
-//
-//  PhotoLibraryManager.swift
-//  Loupe
-//
-//  Created by Jill Allan on 20/07/2025.
-//
-
-import Foundation
 import Photos
+import SwiftUI
+import Observation
 
-struct PhotoLibraryManager {
-    let authorizer: PhotoLibraryAuthorizer
+@Observable
+@MainActor
+class PhotoLibraryManager {
+    @MainActor private(set) var assets: [PHAsset] = []
+    var authorizationStatus: PHAuthorizationStatus = .notDetermined
 
-    func requestAuthorization(_ handler: @escaping (PHAuthorizationStatus) -> Void) async {
-        let status = authorizer.authorizationStatus()
+    func fetchAssets() async {
+        let fetchOptions = PHFetchOptions()
         
-        if status == .notDetermined {
-            let newStatus = await withCheckedContinuation { continuation in
-                authorizer.requestAuthorization { status in
-                    continuation.resume(returning: status)
+        fetchOptions.includeAssetSourceTypes = [
+            .typeUserLibrary,
+            .typeCloudShared
+        ]
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        self.assets = await MainActor.run { () -> [PHAsset] in
+            let result = PHAsset.fetchAssets(
+                with: .image,
+                options: fetchOptions
+            )
+            // Assign results to a temporary assets array
+            // Then assigns this array to the published one
+            // once all updates completed
+            // This ensures all updates are completed at once
+            // avoiding duplicates and multiple UI updates
+            var assets: [PHAsset] = []
+            result.enumerateObjects { asset, _, _ in assets.append(asset) }
+            return Array(assets.prefix(20))
+        }
+    }
+
+    func requestImage(for asset: PHAsset, targetSize: CGSize) async -> Image? {
+        await withCheckedContinuation { continuation in
+            let options = PHImageRequestOptions()
+            options.deliveryMode = .highQualityFormat
+            options.isSynchronous = false
+            options.resizeMode = .exact
+            options.isNetworkAccessAllowed = false
+
+            var didResume = false
+            
+            PHImageManager.default().requestImage(
+                for: asset,
+                targetSize: targetSize,
+                contentMode: .aspectFit,
+                options: options
+            ) { uiImage, _ in
+                
+                guard !didResume else { return }
+                didResume = true
+                #if os(iOS)
+                if let uiImage = uiImage {
+                    continuation.resume(returning: Image(uiImage: uiImage))
+                } else {
+                    continuation.resume(returning: nil)
                 }
+                #elseif os(macOS)
+                if let nsImage = uiImage {
+                    continuation.resume(returning: Image(nsImage: nsImage))
+                } else {
+                    continuation.resume(returning: nil)
+                }
+                #endif
             }
-            handler(newStatus)
-        } else {
-            handler(status)
         }
     }
 }
+
--- Loupe/PhotoView.swift
+++ Loupe/PhotoView.swift
@@ -9,43 +9,100 @@ import Photos
 import SwiftUI
 
 struct PhotoView: View {
-    let photos = [
-        Image(.train1),
-        Image(.train2),
-        Image(.train3),
-        Image(.train1),
-        Image(.train2),
-        Image(.train3),
-        Image(.train2),
-        Image(.train1),
-    ]
+    @State private var photoLibraryManager = PhotoLibraryManager()
 
     @State var selectedIndex: Int = 0
 
     var body: some View {
-        TabView(selection: $selectedIndex) {
-            ForEach(photos.indices, id: \.self) { index in
-                FullscreenPhotoView(photo: photos[index])
-                    .tag(index)
+        NavigationStack {
+            TabView(selection: $selectedIndex) {
+                ForEach(Array(photoLibraryManager.assets.enumerated()), id: \.1) { index, asset in
+                    FullscreenPhotoView(photoLibraryManager: photoLibraryManager, asset: asset)
+                        .tag(index)
+                }
+            }
+//#if os(iOS)
+//            .tabViewStyle(.page(indexDisplayMode: .never))
+//#endif
+            .ignoresSafeArea()
+            .navigationTitle("Photos")
+            .onAppear() {
+                Task {
+                    await photoLibraryManager.fetchAssets()
+                }
             }
         }
-
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .ignoresSafeArea()
     }
-
 }
 
 struct FullscreenPhotoView: View {
-    let photo: Image
+    let photoLibraryManager: PhotoLibraryManager
+    let asset: PHAsset
+    @State private var loadedPhoto: Image?
+    @State private var isHighResLoaded = false
+    @State private var targetSize = CGSize(.zero)
+    @State private var highResTargetSize = CGSize(.zero)
+    @State private var currentZoom = 0.0
+    @State private var totalZoom = 1.0
+    
+    var magnification: some Gesture {
+        MagnifyGesture()
+            .onChanged { value in
+                currentZoom = log(value.magnification)
+                //                if zoomScale > 2.0 && !isHighResLoaded {
+                //                    Task {
+                //                        await photoLibraryManager.requestImage(for: asset, targetSize: highResTargetSize)
+                //                    }
+                //                }
+            }
+            .onEnded { value in
+                totalZoom += currentZoom
+                totalZoom = max(0.5, totalZoom)
+                currentZoom = 0
+            }
+            
+    }
+    
+    @Environment(\.displayScale) var displayScale
 
     var body: some View {
-        ZStack {
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-            photo
-                .resizable()
-                .scaledToFit()
+        GeometryReader { geometry in
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                if let loadedPhoto = loadedPhoto {
+                    loadedPhoto
+                        .resizable()
+                        .scaledToFit()
+                        .scaleEffect(currentZoom + totalZoom)
+                        .gesture(magnification)
+                        .accessibilityZoomAction { action in
+                            if action.direction == .zoomIn {
+                                totalZoom += 1
+                            } else {
+                                totalZoom -= 1
+                            }
+                        }
+                } else {
+                    ProgressView()
+                }
+            }
+            .navigationTitle("Photo")
+            .onAppear {
+                targetSize = CGSize(
+                    width: geometry.size.width * displayScale,
+                    height: geometry.size.height * displayScale
+                )
+                highResTargetSize = CGSize(width: asset.pixelWidth, height: asset.pixelHeight)
+                Task {
+                    if let loaded = await photoLibraryManager.requestImage(for: asset, targetSize: targetSize) {
+                        loadedPhoto = loaded
+                    }
+                }
+            }
+            .onDisappear {
+                totalZoom = 1.0
+            }
         }
     }
 }
